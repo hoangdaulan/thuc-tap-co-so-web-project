017081540648a7980017ad7df9e4feff5cc987a7
hoangdaulan <hoangdaulan@gmail.com>
2026-03-30 16:04:39 +0700

    k

--- updatedb.sql
+++ updatedb.sql
@@ -1,6 +1,14 @@
-INSERT INTO dbo.categories (name, slug, created_at, updated_at)
+-- Cho phép chèn giá trị vào cột Identity (nếu có)
+SET IDENTITY_INSERT dbo.categories ON;
+
+INSERT INTO dbo.categories (id, name, slug, created_at, updated_at)
 VALUES 
-(N'Món chính', 'mon-chinh', GETDATE(), GETDATE()),
-(N'Món ăn vặt', 'mon-an-vat', GETDATE(), GETDATE()),
-(N'Món tráng miệng', 'mon-trang-mieng', GETDATE(), GETDATE()),
-(N'Nước uống', 'nuoc-uong', GETDATE(), GETDATE());
+(1, N'Combo 1 người', 'combo-1-nguoi', GETDATE(), GETDATE()),
+(2, N'Combo nhóm', 'combo-nhom', GETDATE(), GETDATE()),
+(3, N'Gà rán - gà quay', 'ga-ran-ga-quay', GETDATE(), GETDATE()),
+(4, N'Burger - Cơm - Mì ý', 'burger-com-mi-y', GETDATE(), GETDATE()),
+(5, N'Thức ăn nhẹ', 'thuc-an-nhe', GETDATE(), GETDATE()),
+(6, N'Thức uống & tráng miệng', 'thuc-uong-trang-mieng', GETDATE(), GETDATE());
+
+-- Tắt chế độ chèn Identity sau khi xong
+SET IDENTITY_INSERT dbo.categories OFF;
